46bb586828ee107c58e2388fc78a51e166d9fbc8
jnow0819 <jstm1207@gmail.com>
2021-06-15 00:03:50 +0900

    [2021-06-14] '시저 암호' 풀이(수정)

--- Programmers/Step1/시저 암호.py
+++ Programmers/Step1/시저 암호.py
@@ -8,7 +8,7 @@ def solution(s, n):
             answer += i
         # 입력값이 소문자와 대문자 범위를 벗어날 때
         elif 65 <= change_num <= 90 and change_num + n > 90 or change_num + n > 122:
-            answer += chr(ord(i) + n - 26)
+            answer += chr(change_num + n - 26)
         else:
             answer += chr(change_num + n)
     return answer
