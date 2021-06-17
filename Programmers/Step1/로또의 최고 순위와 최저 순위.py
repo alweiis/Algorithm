9a8c7b79d1b4d4c2fd0af10a674198032c5ac02c
jnow0819 <jstm1207@gmail.com>
2021-06-17 14:56:17 +0900

    [2021-06-16] '로또의 최고 순위와 최저 순위' 풀이

---
+++ Programmers/Step1/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,13 @@
+# 코드 수정후 업로드(21-0617)
+def solution(lottos, win_nums):
+    score_table = [6, 6, 5, 4, 3, 2, 1]
+    max_cnt, min_cnt = 0, 0
+    for num in lottos:
+        if num in win_nums:
+            max_cnt += 1
+            min_cnt += 1
+        if num == 0:
+            max_cnt += 1
+    answer = [score_table[max_cnt], score_table[min_cnt]]
+    print(answer)
+    return answer
