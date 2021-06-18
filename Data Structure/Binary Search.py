5fd339b03fa766c70fdadc3540ffcf235e827452
jnow0819 <jstm1207@gmail.com>
2021-06-18 14:54:25 +0900

    [2021-06-18] 'Binary Search' 구현

---
+++ Data Structure/Binary Search.py
@@ -0,0 +1,12 @@
+def solution(L, x):
+    lower = 0
+    upper = len(L) - 1
+    while lower <= upper:
+        middle = (lower + upper) // 2
+        if L[middle] == x:
+            return middle
+        elif L[middle] < x:
+            lower = middle + 1
+        else:
+            upper = middle - 1
+    return -1
