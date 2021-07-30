4da49537941813eb8c4f33276b14f25dcabe9972
jnow0819 <jstm1207@gmail.com>
2021-07-30 11:20:54 +0900

    [2021-07-30] BOJ 5585번 풀이(수정)

--- BOJ/greedy/BOJ_5585.py
+++ BOJ/greedy/BOJ_5585.py
@@ -2,8 +2,7 @@ price = int(input())
 money = 1000 - price
 coin_arr = [500, 100, 50, 10, 5, 1]
 cnt = 0
-while money:
-    for coin in coin_arr:
-        cnt += money // coin
-        money = money % coin
+for coin in coin_arr:
+    cnt += money // coin
+    money = money % coin
 print(cnt)
