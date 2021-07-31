115af763044793d38efead7b995799fb3e368606
jnow0819 <jstm1207@gmail.com>
2021-07-31 16:29:18 +0900

    [2021-07-31] BOJ 1439번 풀이

---
+++ BOJ/greedy/BOJ_1439.py
@@ -0,0 +1,16 @@
+def change_string(x, a, b):
+    cnt = 0
+    for i in range(len(x)):
+        if x[i] == a:
+            x.replace(x[i], b)
+        elif x[i] == b and x[i - 1] == a:
+            cnt += 1
+    print(cnt)
+
+strings = input()
+zero_cnt, one_cnt = strings.count('0'), strings.count('1')
+
+if zero_cnt >= one_cnt:
+    change_string(strings, '1', '0')
+else:
+    change_string(strings, '0', '1')
