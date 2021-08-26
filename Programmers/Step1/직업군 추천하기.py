efd004fb80eaea3a82528869233b75a1f2db29a1
jnow0819 <jstm1207@gmail.com>
2021-08-26 19:03:24 +0900

    [2021-08-26] '직업군 추천하기' 풀이(수정)

--- Programmers/Step1/직업군 추천하기.py
+++ Programmers/Step1/직업군 추천하기.py
@@ -1,19 +1,19 @@
 def solution(table, languages, preference):
     new_table, score_list, result_lst = [], [], []
 
-    for line in table:
-        new_table.append(list(reversed(line.split())))
+    for row in table:
+        new_table.append(list(reversed(row.split())))
 
-    for line in new_table:
-        score = 0
-        for i in range(len(languages)):
-            for j, name in enumerate(line, 1):
-                if languages[i] == name:
-                    score += j * preference[i]
-        score_list.append(score)
+    for row in new_table:
+        total_score = 0
+        for idx in range(len(languages)):
+            for score, name in enumerate(row, 1):
+                if languages[idx] == name:
+                    total_score += score * preference[idx]
+        score_list.append(total_score)
 
-    for i, value in enumerate(score_list):
+    for idx, value in enumerate(score_list):
         if value == max(score_list):
-            result_lst.append(new_table[i][-1])
+            result_lst.append(new_table[idx][-1])
 
     return sorted(result_lst)[0]
