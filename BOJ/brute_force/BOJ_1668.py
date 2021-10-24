2fc0d4e6d5e435e2e07166b64195de7a33cb6196
jnow0819 <jstm1207@gmail.com>
2021-10-24 11:26:07 +0900

    [2021-10-24] BOJ 1668 풀이(수정)

--- BOJ/brute_force/BOJ_1668.py
+++ BOJ/brute_force/BOJ_1668.py
@@ -1,13 +1,12 @@
+def check_trophy():
+    count = 1
+    for i in range(1, n):
+        if max(arr[:i]) < arr[i]:
+            count += 1
+    return count
+
 n = int(input())
-init_lst = [int(input()) for _ in range(n)]
-length = len(init_lst)
-left_cnt, right_cnt = 1, 1
-for idx in range(1, length):
-    if max(init_lst[:idx]) < init_lst[idx]:
-        left_cnt += 1
-init_lst.reverse()
-for idx in range(1, length):
-    if max(init_lst[:idx]) < init_lst[idx]:
-        right_cnt += 1
-print(left_cnt)
-print(right_cnt)
+arr = [int(input()) for _ in range(n)]
+print(check_trophy())
+arr.reverse()
+print(check_trophy())
