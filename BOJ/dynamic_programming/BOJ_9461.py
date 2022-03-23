7d87ad8ef4743d7d97df4b14b2479cf77abddbb3
alweiis <jstm1207@gmail.com>
2022-03-23 10:02:22 +0900

    [2022-03-23] BOJ 9461 풀이

---
+++ BOJ/dynamic_programming/BOJ_9461.py
@@ -0,0 +1,10 @@
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 3]
+    print(dp[n])
