006e1d64d917cf8f65edb719c7433318a67644f6
alweiis <jstm1207@gmail.com>
2022-06-05 17:27:07 +0900

    [2022-06-05] BOJ 1251번 풀이

---
+++ BOJ/brute_force/BOJ_1251.py
@@ -0,0 +1,8 @@
+s = input()
+lst = []
+for i in range(len(s)-2):
+    for j in range(i+1, len(s)-1):
+        for k in range(j+1, len(s)):
+            word = s[:j][::-1] + s[j:k][::-1] + s[k:][::-1]
+            lst.append(word)
+print(sorted(lst)[0])
