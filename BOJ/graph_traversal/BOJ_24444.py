5d518f1a607726e974cf426a731eb878f1883b78
alweiis <jstm1207@gmail.com>
2022-06-06 14:22:14 +0900

    [2022-06-06] BOJ 24444번 풀이

---
+++ BOJ/graph_traversal/BOJ_24444.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(graph, start, visited):
+    global order
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        traversal_order[v] = order
+        order += 1
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for g in graph:
+    g.sort()
+
+visited = [False] * (N+1)
+traversal_order = [0] * (N+1)
+order = 1
+
+bfs(graph, R, visited)
+for i in range(1, len(traversal_order)):
+    print(traversal_order[i])
