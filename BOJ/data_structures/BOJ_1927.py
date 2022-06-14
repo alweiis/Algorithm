70fa339d4b3e6f0d5f0b45262699d3861d891e62
alweiis <jstm1207@gmail.com>
2022-06-14 16:39:50 +0900

    [2022-06-14] BOJ 1972번 풀이

---
+++ BOJ/data_structures/BOJ_1927.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+heap = []
+n = int(input())
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, x)
