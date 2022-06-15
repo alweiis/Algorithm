a6e55aeb1ca67e6b7cc14b66805c5264615762fd
alweiis <jstm1207@gmail.com>
2022-06-15 15:14:38 +0900

    [2022-06-15] BOJ 1753번 풀이

---
+++ BOJ/dijkstra/BOJ_1753.py
@@ -0,0 +1,35 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+V, E = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(V+1)]
+distance = [float('inf')] * (V+1)
+
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, V+1):
+    if distance[i] == float('inf'):
+        print('INF')
+    else:
+        print(distance[i])
