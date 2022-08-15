cf1bad1a3eaa36c7969add74ef9196485a64941e
alweiis <jstm1207@gmail.com>
2022-08-15 23:20:59 +0900

    [2022-08-15] BOJ 11659번 풀이

---
+++ BOJ/prefix_sum/BOJ_11659.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+prefix_sum = [0] * (N + 1)
+for i in range(N):
+    prefix_sum[i+1] = prefix_sum[i] + nums[i]
+for _ in range(M):
+    i, j = map(int, input().split())
+    print(prefix_sum[j] - prefix_sum[i - 1])
