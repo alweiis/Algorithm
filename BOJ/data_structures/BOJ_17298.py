5f96d2a2901a3f023a523e47df422ce207316b54
alweiis <jstm1207@gmail.com>
2022-09-01 22:15:31 +0900

    [2022-09-01] BOJ 17298번 풀이

---
+++ BOJ/data_structures/BOJ_17298.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+answer = [-1] * n
+stack = []
+for i in range(n):
+    while stack and arr[stack[-1]] < arr[i]:
+        answer[stack.pop()] = arr[i]
+    stack.append(i)
+print(*answer)
