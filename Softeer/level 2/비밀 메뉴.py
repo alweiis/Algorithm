79951db2faee87d27ca85428d5caec0d82fae087
alweiis <jstm1207@gmail.com>
2023-10-28 22:34:35 +0900

    [2023-10-28] '비밀 메뉴' 풀이

---
+++ Softeer/level 2/비밀 메뉴.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+m, n, k = map(int, input().split())
+secret_op = list(map(int, input().split()))
+user_op = list(map(int, input().split()))
+answer = 'normal'
+
+if n < m:
+    print(answer)
+else:
+    for i in range(n - m + 1):
+        if user_op[i:i + m] == secret_op:
+            answer = 'secret'
+    print(answer)뉴
