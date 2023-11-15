0a8b25130093ad259a4074d0f20f70f06b267733
alweiis <jstm1207@gmail.com>
2023-11-15 20:12:39 +0900

    [2023-11-15] '피보나치 수' 풀이(v2)

--- Programmers/Step2/피보나치 수.py
+++ Programmers/Step2/피보나치 수.py
@@ -1,9 +1,6 @@
-def solution(num):
-    if num == 1 or num == 2:
-        return 1
-    bottom_up = [0] * (num + 1)
-    bottom_up[1] = 1
-    bottom_up[2] = 1
-    for i in range(3, num + 1):
-        bottom_up[i] = (bottom_up[i-1] + bottom_up[i-2]) % 1234567
-    return bottom_up[-1]
+def solution(n):
+    dp = [0] * 100001
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    return dp[n]
