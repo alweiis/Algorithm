73efb6ae5adc0f36cdd22b74b77a70716fb9e3c7
alweiis <jstm1207@gmail.com>
2023-12-01 09:22:27 +0900

    [2023-12-01] 'PCCE 기출문제 9번' 풀이

---
+++ Programmers/Step1/PCCE 기출문제 9번.py
@@ -0,0 +1,11 @@
+def solution(board, h, w):
+    n, count = len(board), 0
+    dh = [0, 1, -1, 0]
+    dw = [1, 0, 0, -1]
+    for i in range(4):
+        h_check = h + dh[i]
+        w_check = w + dw[i]
+        if 0 <= h_check < n and 0 <= w_check < n:
+            if board[h][w] == board[h_check][w_check]:
+                count += 1
+    return count
