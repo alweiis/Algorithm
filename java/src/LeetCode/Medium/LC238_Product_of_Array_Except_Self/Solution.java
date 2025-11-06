30950f008848fdaaf3c7a1f5be29918d5b1f0cae
alweiis <jstm1207@gmail.com>
2025-11-06 23:30:59 +0900

    [2025-11-06] 238. Product of Array Except Self 풀이

---
+++ java/src/LeetCode/Medium/LC238_Product_of_Array_Except_Self/Solution.java
@@ -0,0 +1,20 @@
+package LeetCode.Medium.LC238_Product_of_Array_Except_Self;
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = 1;
+            if (i > 0) {
+                answer[i] *= nums[i-1] * answer[i-1];
+            }
+        }
+        for (int i = n-2; i >= 0; i--) {
+            answer[i] *= nums[i+1];
+            nums[i] *= nums[i+1];
+        }
+
+        return answer;
+    }
+}
