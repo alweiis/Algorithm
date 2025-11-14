45ba3144e260b19e881b7d279fe2c1747d17f82f
alweiis <jstm1207@gmail.com>
2025-11-14 21:18:14 +0900

    [2025-11-14] 206. Reverse Linked List 풀이

---
+++ java/src/LeetCode/Easy/LC206_Reverse_Linked_List/Solution.java
@@ -0,0 +1,32 @@
+package LeetCode.Easy.LC206_Reverse_Linked_List;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
