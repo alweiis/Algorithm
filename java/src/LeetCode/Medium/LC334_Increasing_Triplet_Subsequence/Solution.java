bfc6caec69c87c8c180346e5a279da0cb98107de
alweiis <jstm1207@gmail.com>
2025-11-18 21:36:12 +0900

    [2025-11-18] 334. Increasing Triplet Subsequence 풀이

---
+++ java/src/LeetCode/Medium/LC334_Increasing_Triplet_Subsequence/Solution.java
@@ -0,0 +1,19 @@
+package LeetCode.Medium.LC334_Increasing_Triplet_Subsequence;
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int x: nums) {
+            if (x <= first) {
+                first = x;
+            } else if (x <= second) {
+                second = x;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
