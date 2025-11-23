272a7e9348be2f53f6310f56a933138d2aa18f6b
alweiis <jstm1207@gmail.com>
2025-11-23 21:23:02 +0900

    [2025-11-23] 1004. Max Consecutive Ones III 풀이(리팩토링)

--- java/src/LeetCode/Medium/LC1004_Max_Consecutive_Ones_III/Solution.java
+++ java/src/LeetCode/Medium/LC1004_Max_Consecutive_Ones_III/Solution.java
@@ -1,35 +1,33 @@
 package LeetCode.Medium.LC1004_Max_Consecutive_Ones_III;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        int output = 0, changed = 0;
+        Deque<Integer> q = new ArrayDeque<>();
+        int maxLen = 0, zeroCount = 0;
 
         for (int num: nums) {
             if (num == 1) {
-                q.offer(num);
+                q.addLast(num);
             } else {
                 if (k == 0) {
-                    while(!q.isEmpty()) {
-                        q.poll();
-                    }
+                    q.clear();
                     continue;
                 }
-                if (changed == k) {
+                if (zeroCount == k) {
                     while (!q.isEmpty() && q.peek() == 1) {
-                        q.poll();
+                        q.pollFirst();
                     }
-                    q.poll();
-                    changed--;
+                    q.pollFirst();
+                    zeroCount--;
                 }
-                q.offer(num);
-                changed++;
+                q.addLast(num);
+                zeroCount++;
             }
-            output = Math.max(output, q.size());
+            maxLen = Math.max(maxLen, q.size());
         }
-        return output;
+        return maxLen;
     }
 }
