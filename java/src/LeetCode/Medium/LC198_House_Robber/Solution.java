0f4d2ac4e08b887b6d70900c683faa31b571f54b
alweiis <jstm1207@gmail.com>
2025-11-27 21:37:59 +0900

    [2025-11-27] 198. House Robber 풀이

---
+++ java/src/LeetCode/Medium/LC198_House_Robber/Solution.java
@@ -0,0 +1,21 @@
+package LeetCode.Medium.LC198_House_Robber;
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[0] + nums[2];
+
+        for (int i = 3; i < n; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3]) + nums[i];
+        }
+
+        return Math.max(dp[n-1], dp[n-2]);
+
+    }
+}
