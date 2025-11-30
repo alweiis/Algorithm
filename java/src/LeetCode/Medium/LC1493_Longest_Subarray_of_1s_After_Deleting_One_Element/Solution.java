e0fb7e4524f563ed165cb7a5a9a5cdbf9fa21e0b
alweiis <jstm1207@gmail.com>
2025-11-30 23:54:05 +0900

    [2025-11-30] 1493. Longest Subarray of 1's After Deleting One Element 풀이

---
+++ java/src/LeetCode/Medium/LC1493_Longest_Subarray_of_1s_After_Deleting_One_Element/Solution.java
@@ -0,0 +1,20 @@
+package LeetCode.Medium.LC1493_Longest_Subarray_of_1s_After_Deleting_One_Element;
+
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int n = nums.length;
+        int l = 0, zeroCount = 0, maxLen = 0;
+
+        for (int r = 0; r < n; r++) {
+            if (nums[r] == 0)   zeroCount++;
+            while (zeroCount > 1) {
+                if (nums[l] == 0) {
+                    zeroCount--;
+                }
+                l++;
+            }
+            maxLen = Math.max(maxLen, r - l);
+        }
+        return (maxLen == n)  ? maxLen - 1 :maxLen;
+    }
+}
