05a95d62f919ddbed6d589600c6ef684b12c6b31
alweiis <jstm1207@gmail.com>
2025-12-21 21:26:46 +0900

    [2025-12-21] 450. Delete Node in a BST 풀이

---
+++ java/src/LeetCode/Medium/LC450_Delete_Node_in_a_BST/Solution.java
@@ -0,0 +1,49 @@
+package LeetCode.Medium.LC450_Delete_Node_in_a_BST;
+
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            // 삭제 대상 발견
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            // 자식이 둘인 경우
+            TreeNode successor = findMin(root.right);
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
