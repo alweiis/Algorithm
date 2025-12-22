ca46c60674f55a2482a34faff6de5dd0b6957904
alweiis <jstm1207@gmail.com>
2025-12-22 23:49:05 +0900

    [2025-12-22] 104. Maximum Depth of Binary Tree 풀이

---
+++ java/src/LeetCode/Easy/LC104_Maximum_Depth_of_Binary_Tree/Solution.java
@@ -0,0 +1,27 @@
+package LeetCode.Easy.LC104_Maximum_Depth_of_Binary_Tree;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
