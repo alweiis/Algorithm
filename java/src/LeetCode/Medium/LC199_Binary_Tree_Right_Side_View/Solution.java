5d86e8f00e1b1c5885262712c43a1379009cd8ab
alweiis <jstm1207@gmail.com>
2026-01-01 23:48:51 +0900

    [2026-01-01] 199. Binary Tree Right Side View 풀이

---
+++ java/src/LeetCode/Medium/LC199_Binary_Tree_Right_Side_View/Solution.java
@@ -0,0 +1,49 @@
+package LeetCode.Medium.LC199_Binary_Tree_Right_Side_View;
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+
+                if (i == levelSize - 1) {
+                    result.add(current.val);
+                }
+            }
+        }
+        return result;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
