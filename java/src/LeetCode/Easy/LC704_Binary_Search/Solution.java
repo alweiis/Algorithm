6d9f8ee160a5d683514cecbf9489d7f40283ef6e
alweiis <jstm1207@gmail.com>
2026-01-06 21:56:12 +0900

    [2026-01-06] 704. Binary Search 풀이

---
+++ java/src/LeetCode/Easy/LC704_Binary_Search/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode.Easy.LC704_Binary_Search;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target)  {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+}
