e83ed865f236b92a5a6a3269d1a26858508c2f95
alweiis <jstm1207@gmail.com>
2026-01-08 21:22:16 +0900

    [2026-01-08] 110. Balanced Binary Tree 풀이

---
+++ java/src/LeetCode/Easy/LC110_Balanced_Binary_Tree/Solution.java
@@ -0,0 +1,37 @@
+package LeetCode.Easy.LC110_Balanced_Binary_Tree;
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private int checkHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
