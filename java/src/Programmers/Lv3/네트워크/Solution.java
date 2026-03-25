d203808521f1889b557c94d3649ca0d10a9b01be
alweiis <jstm1207@gmail.com>
2026-03-25 22:04:55 +0900

    [2026-03-25] '네트워크' 풀이(v2)

--- java/src/Programmers/Lv3/네트워크/Solution.java
+++ java/src/Programmers/Lv3/네트워크/Solution.java
@@ -3,29 +3,43 @@ package Programmers.Lv3.네트워크;
 import java.util.*;
 
 class Solution {
+    boolean[] visited;
+    int[][] computers;
+
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n];
+        visited = new boolean[n];
+        this.computers = computers;
+
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 answer++;
-                bfs(n, computers, i, visited);
+                bfs(i);
             }
         }
         return answer;
     }
 
-    private void bfs(int n, int[][] computers, int i, boolean[] visited) {
+    private void bfs(int i) {
         ArrayDeque<Integer> queue = new ArrayDeque<>();
         queue.add(i);
         visited[i] = true;
         while (!queue.isEmpty()) {
             int curr = queue.poll();
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < computers.length; j++) {
                 if (!visited[j] && computers[curr][j] == 1) {
                     queue.add(j);
                     visited[j] = true;
                 }
+            }
+        }
+    }
+
+    private void dfs(int curr) {
+        visited[curr] = true;
+        for (int j = 0; j < computers.length; j++) {
+            if (!visited[j] && computers[curr][j] == 1) {
+                dfs(j);
             }
         }
     }
